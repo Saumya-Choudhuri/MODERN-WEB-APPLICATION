4832d1c9dd63ade30f7832d1f7c0399fa67841c3
Saumya-Choudhuri <saumya.choudhuri123@gmail.com>
2025-04-02 23:08:57 +0530

    Removed all files from repository but kept them locally

--- .vscode/ASSIGNMENT01/3) real time character count/script.js
+++
@@ -1,6 +0,0 @@
-let text = document.getElementById("textInput"), count = document.getElementById("charCount");
-text.addEventListener("input", () => {
-    let len = text.value.length;
-    count.textContent = `${len} / 150`;
-    count.classList.toggle("red", len > 150);
-});
